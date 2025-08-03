abf14346957920ca483a83b4e67ec3e57f6151c8
Mohamed shazan <90765408+Mohamedshazan@users.noreply.github.com>
2025-08-03 23:01:25 +0530

    Update api.ts

--- frontend/src/app/lib/api.ts
+++ frontend/src/app/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://asset-backend-production-00f6.up.railway.app/api', 
+  baseURL: 'asset-backend-production-acb2.up.railway.app', 
  
   headers: {
     Accept: 'application/json',
